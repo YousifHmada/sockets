28d427a5a1bec99bfe57c2cf485286868a0d7759
YousifHmada <1401724@eng.asu.edu.eg>
2017-09-29 10:28:43 +0200

    =broadcasting users

--- server/server.js
+++ server/server.js
@@ -16,10 +16,25 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
 	console.log('new user connected');
+	socket.emit('newMessage',{
+			from:"admin",
+			text:"welcome new user",
+			createdAt:new Date().getTime()
+		});
+	socket.broadcast.emit('newMessage',{
+			from:"admin",
+			to:"new user arrived",
+			createdAt:new Date().getTime()
+		});
 	socket.on('newMessage',(newMessage)=>{
-		io.emit('newMessage',{
+		// io.emit('newMessage',{
+		// 	from:newMessage.from,
+		// 	to:newMessage.text,
+		// 	createdAt:new Date().getTime()
+		// });
+		socket.broadcast.emit('newMessage',{
 			from:newMessage.from,
-			to:newMessage.to,
+			text:newMessage.text,
 			createdAt:new Date().getTime()
 		});
 	});
